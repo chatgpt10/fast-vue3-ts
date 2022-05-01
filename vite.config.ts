e407edd4c734f2708ce48202ec74392f2bd5685c
LIU <1913698873@qq.com>
2022-05-01 17:58:59 +0800

    feat:Eslint-init

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 // npm install @types/node --save-dev  处理报错
 
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]  
-})
+  plugins: [vue()],
+});
