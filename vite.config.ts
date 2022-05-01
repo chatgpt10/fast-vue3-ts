1a7d1d8768ddd5220ee95d4e9eae80ab411a8005
LIU <1913698873@qq.com>
2022-05-01 17:13:26 +0800

    FAST-VUE3-TS  INIT

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// npm install @types/node --save-dev  处理报错
+
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]  
+})
