f93112ad116e5abfd28570a4aa9db30df6d13411
LIU <1913698873@qq.com>
2022-05-01 18:08:01 +0800

    feat:配置@别名

--- .eslintrc.js
+++ .eslintrc.js
@@ -119,7 +119,7 @@ module.exports = {
     // 禁止出现多行空行
     "no-multiple-empty-lines": "warn",
     // 禁止出现;
-    semi: ["warn", "never"],
+    // semi: ["warn", "never"],
     // 强制在块之前使用一致的空格
     "space-before-blocks": "warn",
     // 强制在 function的左括号之前使用一致的空格
