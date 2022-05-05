f3445f9ff29e3630ab27d2b261e1002033aaf326
LIU <1913698873@qq.com>
2022-05-05 16:14:20 +0800

    feat:配置Token处理文件

---
+++ src/utils/auth.ts
@@ -0,0 +1,20 @@
+// 处理token
+const TokenKey = "FAST-VUE3-TS";
+const TokenPrefix = "Bearer"; // token前缀
+// 登陆状态
+const isLogin = () => {
+  return !!localStorage.getItem(TokenKey);
+};
+// 设置token
+const setToken = (token: string) => {
+  return localStorage.setItem(TokenKey, token);
+};
+// 获取token
+const getToken = () => {
+  return localStorage.getItem(TokenKey);
+};
+// 清除token
+const clearToken = () => {
+  return localStorage.removeItem(TokenKey);
+};
+export { TokenPrefix, isLogin, setToken, getToken, clearToken };
