49c40c885bcd0fabd0bebc3047e6f5dd926e44ec
LIU <1913698873@qq.com>
2022-05-09 18:18:36 +0800

    feat:完成Home Card封装

--- src/components/InfoDescription/index.vue
+++ src/components/InfoDescription/index.vue
@@ -1,58 +1,71 @@
 <template>
-    <!-- Home页描述卡片 -->
-    <div class="infoDescriptionCard">
-        <div class="infoHeader">
-            <SvgIcon name="viteLogo" />
-        </div>
-        <div class="infoBody">
-            <div>
-                <h3 style="display:inline-block">Vite2.8</h3>
-                <a href="" style="float:right">
-                    <SvgIcon name="GitHub" />
-                </a>
-            </div>
-
-            <span>
-                下一代前端开发与构建工具,极速的服务启动,轻量快速的热重载,丰富的功能...
-            </span>
-        </div>
+  <!-- Home页描述卡片 -->
+  <div class="infoDescriptionCard">
+    <div class="infoHeader">
+      <SvgIcon :name="cardInfoData.cardSvgName" />
     </div>
+    <div class="infoBody">
+      <div>
+        <h3 style="display: inline-block">{{ cardInfoData.cardName }}</h3>
+        <a :href="cardInfoData.cardHref" style="float: right">
+          <SvgIcon name="GitHub" />
+        </a>
+      </div>
+      <span>
+        {{ cardInfoData.cardDes }}
+      </span>
+    </div>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, reactive } from "vue";
+const props = defineProps({
+  cardInfoData: {
+    type: Object,
+    default: null, 
+  },
+});
+console.log(props.cardInfoData);
+</script>
 <style scoped lang="less">
 .infoDescriptionCard {
-    border-radius: 10px;
+  border-radius: 10px;
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  height: 100%;
+  flex-direction: column;
+  overflow: hidden;
+  border: 1px solid #ddd;
+  box-shadow: 2px 0px #c5c5c5;
+
+  .infoHeader {
+    height: 250px;
     display: flex;
-    align-items: stretch;
-    justify-content: center;
-    height: 100%;
-    flex-direction: column;
-    overflow: hidden;
-    border: 1px solid #ddd;
-    box-shadow: 2px 0px #c5c5c5;
 
-
-    .infoHeader {
-        height: 250px;
-        display: flex;
-
-        .svg-icon-spin {
-            width: 150px;
-            height: 100%;
-            margin: 0 auto;
-        }
-
+    .svg-icon-spin {
+      width: 150px;
+      height: 100%;
+      margin: 0 auto;
     }
+  }
 
-    .infoBody {
-        flex: 1;
-        padding: 10px;
+  .infoBody {
+    flex: 1;
+    padding: 10px;
+    /*
         background-image: linear-gradient(to right, #51bcff, #b241fe);
+        */
+    background-image: linear-gradient(
+      to right,
+      v-bind("cardInfoData.carColorFirst"),
+      v-bind("cardInfoData.carColorLast")
+    );
 
-        .svg-icon-spin {
-            width: 30px;
-            margin: 0 auto;
-        }
+    .svg-icon-spin {
+      width: 30px;
+      margin: 0 auto;
     }
+  }
 }
 </style>
--- src/pages/Home/components/home/index.vue
+++ src/pages/Home/components/home/index.vue
@@ -2,16 +2,16 @@
   <div class="homeContainer">
     <a-row class="grid-demo" :gutter="{ md: 8, lg: 24, xl: 32 }">
       <a-col :span="4">
-        <InfoDescription />
+        <InfoDescription :cardInfoData="cardInfoData" />
       </a-col>
       <a-col :span="4">
-        <div>col - 4</div>
+        <InfoDescription :cardInfoData="cardInfoData" />
       </a-col>
       <a-col :span="4">
-        <div>col - 4</div>
+        <InfoDescription :cardInfoData="cardInfoData" />
       </a-col>
       <a-col :span="4">
-        <div>col - 4</div>
+        <InfoDescription :cardInfoData="cardInfoData" />
       </a-col>
       <a-col :span="4">
         <div>col - 4</div>
@@ -24,7 +24,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref, reactive } from "vue";
 import InfoDescription from "@/components/InfoDescription/index.vue";
+// 定义props数据
+const cardInfoData = reactive({
+  cardSvgName: "viteLogo",
+  cardName: "Vite2.9",
+  cardHref: "https://github.com/vitejs/vite",
+  cardDes:
+    "下一代前端开发与构建工具,极速的服务启动,轻量快速的热重载,丰富的功能...",
+  carColorFirst: "#51bcff",
+  carColorLast: "#b241fe",
+});
 </script>
 
 <style lang="less" scoped>
