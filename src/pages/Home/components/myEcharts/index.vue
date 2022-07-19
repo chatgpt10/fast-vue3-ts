d95cc49fb6846923b80f27ad53ba48b0c60b0d49
liuerjian <1913698873@qq.com>
2022-07-19 17:54:10 +0800

    feat:添加饼状图

--- src/pages/Home/components/myEcharts/index.vue
+++ src/pages/Home/components/myEcharts/index.vue
@@ -1,7 +1,8 @@
 <template>
 	<el-row :gutter="5">
-		<el-col :span="6"><div class="echarts" id="main" style="height: 300px; width: 100%"></div></el-col>
-		<el-col :span="6"><div class="echarts" id="main2" style="height: 300px; width: 300px"></div></el-col>
+		<el-col :span="6"><div class="echarts" id="mainBar" style="height: 400px; width: 100%"></div></el-col>
+		<el-col :span="6"><div class="echarts" id="mainPie" style="height: 400px; width: 100%"></div></el-col>
+
 		<el-col :span="6"><div class="echarts" id="main" style="height: 300px; width: 300px"></div></el-col>
 		<el-col :span="6"><div class="echarts" id="main" style="height: 300px; width: 300px"></div></el-col>
 	</el-row>
@@ -9,18 +10,39 @@
 
 <script setup lang="ts">
 import * as echarts from "echarts/core";
-import { GridComponent, GridComponentOption } from "echarts/components";
-import { BarChart, BarSeriesOption } from "echarts/charts";
+import {
+	GridComponent,
+	GridComponentOption,
+	TooltipComponent,
+	ToolboxComponent,
+	ToolboxComponentOption,
+	LegendComponent,
+	LegendComponentOption
+} from "echarts/components";
+import { BarChart, BarSeriesOption, PieChart, PieSeriesOption } from "echarts/charts";
+import { LabelLayout } from "echarts/features";
 import { CanvasRenderer } from "echarts/renderers";
 //注意这里引入了onMounted这个钩子，等会有用
 import { onMounted } from "vue";
 import { useEcharts } from "@/hooks/useEcharts";
-echarts.use([GridComponent, BarChart, CanvasRenderer]);
+echarts.use([
+	GridComponent,
+	BarChart,
+	CanvasRenderer,
+	TooltipComponent,
+	ToolboxComponent,
+	LegendComponent,
+	PieChart,
+	CanvasRenderer,
+	LabelLayout
+]);
 
-type EChartsOption = echarts.ComposeOption<GridComponentOption | BarSeriesOption>;
-
-const echartsInit = () => {
-	let chartDom = document.getElementById("main")!;
+type EChartsOption = echarts.ComposeOption<
+	GridComponentOption | BarSeriesOption | ToolboxComponentOption | LegendComponentOption | PieSeriesOption
+>;
+// 柱状图
+const echartsInitBar = () => {
+	let chartDom = document.getElementById("mainBar")!;
 	let myChart = echarts.init(chartDom);
 	let option: EChartsOption;
 
@@ -37,20 +59,56 @@ const echartsInit = () => {
 				data: [120, 200, 150, 80, 70, 110, 130],
 				type: "bar"
 			}
-		]
+		],
+		tooltip: {}
 	};
-
-	// option && myChart.setOption(option);
-	// window.onresize = function () {
-	// 	//自适应大小, 不用的话不会自适应大小。
-	// 	myChart.resize();
-	// };
 
 	useEcharts(myChart, option);
 };
+// 饼状图
+const echartsInitPie = () => {
+	let chartDom = document.getElementById("mainPie")!;
+	let myChart = echarts.init(chartDom);
+	let option: EChartsOption;
 
+	option = {
+		legend: {
+			top: "bottom"
+		},
+		toolbox: {
+			show: true,
+			feature: {
+				mark: { show: true },
+				dataView: { show: true, readOnly: false },
+				restore: { show: true },
+				saveAsImage: { show: true }
+			}
+		},
+		series: [
+			{
+				name: "Nightingale Chart",
+				type: "pie",
+				radius: [20, 100],
+				center: ["50%", "50%"],
+				roseType: "area",
+				itemStyle: {
+					borderRadius: 8
+				},
+				data: [
+					{ value: 40, name: "rose 1" },
+					{ value: 38, name: "rose 2" },
+					{ value: 32, name: "rose 3" },
+					{ value: 30, name: "rose 4" },
+					{ value: 28, name: "rose 5" }
+				]
+			}
+		]
+	};
+	useEcharts(myChart, option);
+};
 onMounted(() => {
-	echartsInit();
+	echartsInitBar();
+	echartsInitPie();
 });
 </script>
 
